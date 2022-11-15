975c603cc1242c00816d9a0cb72ef227b4a0f4fd
Kaur <kaur.madisson@tptlive.ee>
2022-11-15 11:32:26 +0200

    kastid

--- questions.js
+++ questions.js
@@ -1,16 +1,50 @@
 import inquirer from 'inquirer';
+
 inquirer
     .prompt([
-{type: 'input', name: 'age',message: 'How old are you?'},
-{type: 'input', name: 'gender',message: 'How gender are you?'},
-{type: 'input', name: 'name',message: 'What is your name?'},
-{type: 'input', name: 'height',message: 'How tall are you?'},        
+{type: 'input', name: 'age',message: ''},
+{type: 'confirm', name: 'värv',message: 'Kas sulle meeldib sinine värv'},
+{type: 'password', name: 'parool',message: 'Kas sisestatud password on õige?', mask: ''},
+{type: 'checkbox', name: 'checkbox',message: 'pitsa burger?', choices:[
+    'pitsa',
+    'burger',
+]}, 
+
+{type: 'number', name: 'number',message: 'How old are you'},
+{type: 'list', name: 'list ',message: 'What kind of pizza do you want', choices:[
+    "pepperoni",
+    "kebab",
+]},
+{type: 'rawlist', name: 'rawlist',message: 'How much money do you have?', choices:[
+    '18',
+    '15',
+    '12',
+    '9',
+]},
+{type: 'expand', name: 'expand',message: 'What to change?',  choices: [
+    
+    {
+      key: 'y',
+      name: 'Overwrite',
+      value: 'Change few things',
+    },
+   
+  
+]},
+
+{type: ' editor', name: 'editor',message: ''},
+
 ])
 
 .then((answer)=> {
     console.log(' I am ' + answer.age + ' years old');
-    console.log(' I am ' + answer.gender + '');
-    console.log(' I am ' + answer.name + '');
-    console.log(' I am ' + answer.height + ' centimeters');
+    console.log('  ' + answer.värv + '');
+    console.log(' My password is ' + answer.parool + '');
+    console.log(`  I like  ${answer.checkbox} ` );
+    console.log(`  I am  ${answer.number} ` );
+    console.log(`  I have  ${answer.rawlist} ` );
+    console.log(`  I have to  ${answer.expand} ` );
+    console.log(`    ${answer.editor} ` );
+    
 })
 
